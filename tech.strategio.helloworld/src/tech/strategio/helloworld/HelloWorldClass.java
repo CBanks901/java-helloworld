3c8b9aed718cf3be24b1d58bba78ba926c893311
CBanks901 <christianbanks@ymail.com>
2022-04-14 15:21:06 -0500

    Updated hello world package

--- tech.strategio.helloworld/src/tech/strategio/helloworld/HelloWorldClass.java
+++ tech.strategio.helloworld/src/tech/strategio/helloworld/HelloWorldClass.java
@@ -4,7 +4,7 @@ public class HelloWorldClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Hello World");
+		System.out.println("Hello World");
 	}
 
 }
